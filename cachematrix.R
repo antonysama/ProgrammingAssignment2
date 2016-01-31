bc51561c96f81dbec51f770916a587f7d7c71a03
Antony Samarawickrema <antonysama@gmail.com>
2016-01-31 13:57:34 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,29 @@
 ## Goal is to cache potentially time-consuming computations. With 2 funtions.
 
-## makeCacheMatrix: creates a special "matrix" object that can cache its inverse.
-                  # Creating a set of random numbers to be passed down to makeCacheMatrix
-                  d<-rnorm(16) #data for the matrix
-                  r = 4 #rows in matrix
-                  c = 4 #columns 
 makeCacheMatrix <- function(x = matrix(x)) {
-                  x<-matrix(d,r,c)
-}
-                  
-##inverting the special matrix
-                  inv<-solve(x)
-
-##caching the inverted matrix
-                  c<<-inv
-
+    i<-NULL #set the cache (inverse matrix) NULL at the start of running the code.
+    set<-function(y){ #subfunction that makes & sets the matrix
+        x<<-y 
+        i<<-NULL
+   }
+    get<-function () x #get the value of the vector
+    setmatrix<-function(matrix) i<<-set #set the matrix to invert
+    getmatrix ()<-function () i #get the inverted matrix 
+    list(set=set, get=get, #setting the getters & setters
+        setmatrix=setmatrix, 
+        getmatrix=getmatrix) 
 ## cacheSolve: computes the inverse of the special "matrix" returned  above. 
 ##If the inverse has already been calculated (and the matrix has not changed), 
 ## then the cacheSolve() should retrieve the inverse from the cache.
                   
-cacheSolve <- function(y) {
+cacheSolve <- function(x,...) {
 ## If the cache containts it, return a matrix that is the inverse of 'x'
-  
-                   if(!is.null (c)) {
-                   message("getting cached data")
-                   return(c)
-                   }
-## if the cache is null or empy then compute the inverse
-                  solve(x)
+    i<-x$getmatrix() getting inverted matrix from cache
+    if(!is.null (i)) { #if the cache isn't empty then...
+        message("getting cached data")
+        return(i)
+        }
+## this is like an "else", if the cache is empty then compute the inverse
+    get(x) #get matrix to be invereted    
+    solve(x) #invert the matrix
 }
